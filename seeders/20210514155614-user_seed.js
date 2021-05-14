e279377f9c77911ed92eff44ed86d8c4199d474c
Khachik Galstyan <khach.galstyan@gmail.com>
2021-05-14 20:03:07 +0400

    Added seeder

---
+++ seeders/20210514155614-user_seed.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
+
+        await queryInterface.bulkInsert('User', [{
+            google_id: '1234566778',
+            first_name: 'John',
+            last_name: 'Doe',
+            email: 'john@xample.com'
+        }], {});
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        await queryInterface.bulkDelete('User', null, {});
+    }
+};
